a6ba0cea0d8fdfec9384c9ece2e15049d61c084d
Yogesh Basnet <basnetyogesh@gmail.com>
2016-04-03 17:21:46 +0545

    before enabling migration

---
+++ JaywayTutorial/Models/MyAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace JaywayTutorial.Models
+{
+    public class MyAppContext:DBContext
+    {
+        public MyAppContext()
+            : base("MyAppContext")
+        {
+
+        }
+
+        public DbSet<Thing> Things { get; set; }
+    }
+}
